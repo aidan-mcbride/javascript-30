6c6e6eaee9d6b029dc2898c6b37f4f0c1a581914
aidan-mcbride <aidanmcbride@outlook.com>
2019-10-18 12:40:45 -0600

    Day 17 complete - sort without articles
    refactored after watching video

--- 17-sort-without-articles/script.js
+++ 17-sort-without-articles/script.js
@@ -1,29 +1,12 @@
-const bandsList = document.querySelector('#bands');
-
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-sortedBands = bands.sort((a, b) => {
-  const rA = removeArticles(a);
-  const rB = removeArticles(b);
-  if (rA > rB) { return 1 }
-  if (rA < rB) { return -1 }
-  return 0;
-})
+const sortedBands = bands.sort((a, b) => removeArticles(a) > removeArticles(b) ? 1 : -1);
 
 function removeArticles(band) {
-  const articles = ['a', 'an', 'the'];
-  const words = band.split(' ');
-  if (articles.includes(words[0].toLowerCase())) {
-    words.shift();
-    return words.join(' ');
-  }
-  return band;
+  return band.replace(/^(a |the |an )/i, '').trim();
 }
 
-function populateList(list = [], listEl) {
-  listEl.innerHTML = list.map(band => {
-    return `<li>${band}</li>`
-  }).join('');
-}
 
-populateList(list=sortedBands, listEl=bandsList)
+const bandsList = document.querySelector('#bands');
+
+bandsList.innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('');
